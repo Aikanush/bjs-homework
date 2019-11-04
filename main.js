6885af193cc4f852d60e25cd9d9c75fa9fb1cea3
Aikanush <haka.m@mail.ru>
2019-11-04 18:02:34 +0400

    homework

---
+++ main.js
@@ -0,0 +1,20 @@
+// Задание 1
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+console.log((algebra + geography + physics) / 3);
+
+// Задание 2
+
+myName = 'Айкануш';
+message = 'Привет, мир! Меня зовут ' + myName;
+console.log(message);
+
+// Задание 3
+
+x = 2;
+y = 22;
+z = 0;
+result = (x * y) + (5 * z) + (x - 1);
+console.log(result);
