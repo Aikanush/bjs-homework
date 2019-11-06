4fc3e5994a675f5ce3351675ee600ed6bdb31618
Aikanush <haka.m@mail.ru>
2019-11-06 23:09:39 +0400

    changed main.js

--- main.js
+++ main.js
@@ -3,18 +3,19 @@
 let algebra = 5;
 let geography = 4;
 let physics = 4;
-console.log((algebra + geography + physics) / 3);
+let averageMarks = algebra + geography + physics / 3;
+console.log(averageMarks);
 
 // Задание 2
 
-myName = 'Айкануш';
-message = 'Привет, мир! Меня зовут ' + myName;
+let myName = 'Айкануш';
+let message = `Привет, мир! Меня зовут ${myName}`;
 console.log(message);
 
 // Задание 3
 
-x = 2;
-y = 22;
-z = 0;
-result = (x * y) + (5 * z) + (x - 1);
+let x = 2;
+let y = 22;
+let z = 0;
+let result = (x * y) + (5 * z) + (x - 1);
 console.log(result);
